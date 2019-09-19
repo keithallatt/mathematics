9aee2632a318e57e3e628ed14373c6101917c050
Keith Allatt <keith.allatt@gmail.com>
2019-09-19 14:31:49 -0400

    Added functions

--- Matrix.java
+++ Matrix.java
@@ -2,8 +2,67 @@
 
 public class Matrix {
 	double matrix[][];
+	int width, height;
 
 	public Matrix(int width, int height) {
-		matrix = new double[width][height];
+		this.width = width;
+		this.height = height;
+		matrix = new double[height][width];
+	
+		for (int w = 0; w < width; w++)
+			for (int h = 0; h < height; h++)
+				matrix[h][w] = 0;
+	}
+	
+	public double determinant() {
+		if (width != height)
+			throw new RuntimeException("Non-square matrices do not have determinants");
+		int dim = width;
+		
+		if (dim == 1)
+			return matrix[0][0];
+
+		double running_total = 0;
+		
+		for (int i = 0; i < dim; i++) {
+			double scalar = matrix[0][i];
+
+			Matrix minor = new Matrix(dim-1, dim-1);
+			for (int h = 1; h < dim; h++) {
+				for (int a = 0; a < i; a++)
+					minor.matrix[h-1][a] = matrix[h][a];
+				for (int a = i+1; a < dim; a++)
+					minor.matrix[h-1][a-1] = matrix[h][a];
+				
+			}
+
+			running_total += Math.pow(-1, i+2) * scalar * minor.determinant(); 
+		}
+		return running_total;
+	}
+
+	public static Matrix identity(int dim) {
+		Matrix m = new Matrix(dim, dim);
+		for (int i = 0; i < dim; i++)
+			m.set(i,i,1);
+
+		return m;
+	}
+
+	public void set(int i, int j, double value) {
+		matrix[i][j] = value;
+	}
+
+	public double get(int i, int j) {
+		return matrix[i][j];
+	}
+
+	public Vector vectorization() {
+		Vector v = new Vector(width*height);
+		for (int j = 0; j < width; j++)
+			for (int i = 0; i < height; i++)
+				v.set(j*height + i,  matrix[i][j]);
+		return v;
 	}
 }
+
--- Vector.java
+++ Vector.java
@@ -17,7 +17,50 @@ public class Vector {
 		return v;
 	}
 
-	public double dotProduct(Vector other) {
+	public void set(int dim, double value) {
+		vector[dim] = value;
+	}
+
+	public double get(int dim) {
+		return vector[dim];
+	}
+
+	public Vector add(Vector other) {
+		if (other.dimension != dimension)
+			throw new RuntimeException("Vectors must have same dimension");
+		
+		Vector sum = new Vector(dimension);
+		for (int i = 0; i < dimension; i++)
+			sum.vector[i] = vector[i] + other.vector[i];
+		return sum;
+	}
+
+
+	public Vector subtract(Vector other) {
+		if (other.dimension != dimension)
+			throw new RuntimeException("Vectors must have same dimension");
+		
+		Vector difference = new Vector(dimension);
+		for (int i = 0; i < dimension; i++)
+			difference.vector[i] = vector[i] - other.vector[i];
+		return difference;
+	}
+
+
+	public Vector scalarMultiply(double scalar) {
+		Vector multiple = new Vector(dimension);
+		for (int i = 0; i < dimension; i++)
+			multiple.vector[i] = vector[i] * scalar;
+
+		return multiple;
+	}
+
+	/*
+	 * Cross product only exists in R^0, R^1, R^3 and R^7 as a function from R^n X R^n -> R^n, so we will define as [ R^n ]^(n-1) -> R^n
+	 */
+	
+
+	public double innerProduct(Vector other) {
 		if (other.dimension != dimension)
 			throw new RuntimeException("Vectors must have same dimension");
 		
@@ -25,5 +68,15 @@ public class Vector {
 		for (int i = 0; i < dimension; i++)
 			total += vector[i] * other.vector[i];
 		return total;
+	}
+
+	public Matrix outerProduct(Vector other) {
+		Matrix m = new Matrix(dimension, other.dimension);
+
+		for (int i = 0; i < dimension; i++)
+			for (int j = 0; j < other.dimension; j++)
+				m.set(i, j, vector[i]*other.vector[j]);
+		
+		return m;
 	}
 }	
