890d8232a16cf08d54cd212da276bef2d4e32a9b
Keith Allatt <keith.allatt@gmail.com>
2019-09-22 12:52:26 -0400

    Added documentation and add element function signature

--- sets/Set.java
+++ sets/Set.java
@@ -1,5 +1,17 @@
 
 
 public abstract class Set<T> {
+	
+	
+
+	/**
+	 * Containment function. Returns true if element is in this set.
+	 * For infinite sets, this function is not as easy as checking if
+	 * the element is in an array.
+	 */
 	public abstract boolean contains(T element);
+
+	public void addElement(T element) {
+
+	}
 }
